a7f18ad9c505c23d1d3a0c9441d62255817d2399
BigSellRollTee <70051134+BigSellRollTee@users.noreply.github.com>
2020-10-03 13:36:03 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const db = require("./app/models");
+db.sequelize.sync();
+
+app.get("/",(req, res) => {
+    res.json({message: "Welcome to my Application."});
+});
+
+require("./app/routes/tutorial.routes")(app)
+
+app.listen(8080, () => {
+    console.log("Server Start at Port 8080")
+});
